d03045ee981ec46519300ff4069554cd0d1af61a
jintiao <jintiao@outlook.com>
2016-08-12 16:44:23 +0800

    render flow

--- cobra.cpp
+++ cobra.cpp
@@ -1,44 +1,23 @@
 #include <fstream>
+#include <functional>
 #include <sstream>
 #include <string>
 #include <vector>
 
-//struct Color { unsigned char b, g, r, a; };
+struct Color { unsigned char b, g, r, a; };
 struct Vector2 { float u, v; };
 struct Vector3 { float x, y, z; };
-struct Index { int vertex, uv, normal; };
-struct Index3 { Index i[3]; };
-//struct Vector4 { float x, y, z, w; }
-//struct Vertex { float x, y, z, u, v; };
-
-//struct renderer {
-//    renderer (int size) {
-//        frameBuffer.resize (size);
-//        for (auto &px : frameBuffer) px = {255, 255, 255, 0};
-//        depthBuffer.resize (size);
-//        for (auto &d : depthBuffer) d = std::numeric_limits<float>::max();
-//    }
-//    void Draw () {
-//    }
-//    std::vector<Color> frameBuffer;
-//    std::vector<float> depthBuffer;
-//};
-//
-//void VertexShader (const Matrix4 &mvp, const Vertex &vertex, Vector4 &outPos) {
-//    Vector4 pos = mvp * Vector4{ vertex.x, vertex.y, vertex.z, 1.0f };
-//}
-//
-//void PixelShader (Color &outColor) {
-//}
+struct Index { int pos, uv, normal; };
+struct Index3 { Index index[3]; };
+struct Vertex { Vector3 pos, normal; Vector2 uv; };
 
 struct Model {
-	std::vector<Vector3> vertexBuffer;
+	std::vector<Vector3> posBuffer;
 	std::vector<Vector3> normalBuffer;
 	std::vector<Vector2> uvBuffer;
 	std::vector<Index3> indexBuffer;
-	Model (std::string str) {
+	Model (std::string str, char dummy = '/') {
 		float x, y, z;
-		char c;
 		std::ifstream is (str);
 		while (std::getline (is, str)) {
 			if (str.length () < 2) continue;
@@ -54,27 +33,71 @@ struct Model {
 			}
 			else if (str[0] == 'v') {
 				iss >> token >> x >> y >> z;
-				vertexBuffer.push_back ({ x, y, z });
+				posBuffer.push_back ({ x, y, z });
 			}
 			else if (str[0] == 'f') {
-				Index3 index;
-				iss >> token >> index.i[0].vertex >> c >> index.i[0].uv >> c >> index.i[0].normal >> 
-					index.i[1].vertex >> c >> index.i[1].uv >> c >> index.i[1].normal >> 
-					index.i[2].vertex >> c >> index.i[2].uv >> c >> index.i[2].normal;
-				indexBuffer.push_back (index);
+				Index3 indexes;
+				iss >> token >> indexes.index[0].pos >> dummy >> indexes.index[0].uv >> dummy >> indexes.index[0].normal >>
+					indexes.index[1].pos >> dummy >> indexes.index[1].uv >> dummy >> indexes.index[1].normal >>
+					indexes.index[2].pos >> dummy >> indexes.index[2].uv >> dummy >> indexes.index[2].normal;
+				indexBuffer.push_back (indexes);
 			}
 		}
 	}
 };
 
+const int WIDTH = 1024, HEIGHT = 768;
+struct Renderer {
+	std::vector<Color> frameBuffer;
+	std::vector<float> depthBuffer;
+	Renderer () : frameBuffer (WIDTH * HEIGHT, { 255, 255, 255, 0 }), depthBuffer (WIDTH * HEIGHT, std::numeric_limits<float>::max ()) { }
+	void DrawModel (Model &model) {
+		auto VertexShader = [] (const Vector3 &pos, const Vector3 &normal, const Vector2 &uv, Vertex &outVertex) {
+			outVertex.pos = pos;
+			outVertex.normal = normal;
+			outVertex.uv = uv;
+		};
+		for (auto &indexes : model.indexBuffer) {
+			Vertex ov[3];
+			for (int i = 0; i < 3; i++) {
+				VertexShader (model.posBuffer[indexes.index[i].pos - 1], model.normalBuffer[indexes.index[i].normal - 1], model.uvBuffer[indexes.index[i].uv - 1], ov[i]);
+				DrawTriangle (ov[0], ov[1], ov[2]);
+			}
+		}
+	}
+	void DrawTriangle (const Vertex &v0, const Vertex &v1, const Vertex &v2) {
+		DrawPoint (v0);
+		DrawPoint (v1);
+		DrawPoint (v2);
+	}
+	void DrawPoint (const Vertex &v) {
+		auto PixelShader = [] (const Vector2 &uv, Color &outColor) {
+			outColor = { 255, 0, 0, 0 };
+		};
+		Color c;
+		int x = (int)(v.pos.x * WIDTH / 2.0f), y = (int)(v.pos.y * HEIGHT / 2.0f);
+		int p = x + y * WIDTH;
+		if (x >= 0 && y >= 0 && p < frameBuffer.size ()) {
+			PixelShader (v.uv, c);
+			frameBuffer[p] = c;
+		}
+	}
+};
+
+void SaveBmp (std::vector<Color> &frameBuffer, int width, int height, std::string file) {
+#define INT2CHAR_BIT(num, bit) (char)(((num) >> (bit)) & 0xff)
+#define INT2CHAR(num) INT2CHAR_BIT((num),0), INT2CHAR_BIT((num),8), INT2CHAR_BIT((num),16), INT2CHAR_BIT((num),24)
+	const char header[54] = { 'B', 'M', INT2CHAR(54+width*height*sizeof(Color)), INT2CHAR(0), INT2CHAR(54), INT2CHAR(40), INT2CHAR(width), INT2CHAR (height), 1, 0, 32, 0 };
+	std::ofstream ofs (file, std::ios_base::out | std::ios_base::binary);
+	ofs.write (header, sizeof(header));
+	ofs.write (static_cast<const char *>(static_cast<const void *>(frameBuffer.data ())), width*height * sizeof (Color));
+}
 
 int main (int argc, char **argv) {
 	Model model (argc > 1 ? argv[1] : "cube.obj");
-    //Renderer renderer (1024, 768);
-    //Model model = LoadModel ();
-    //renderer.Bind (model);
-    //Matrix4 mvp = CreateProjectMatrix (1.57f, 1024.0f / 768.0f, 0.1f, 1000.0f) * CreateViewMatrix ({4.0f, 3.0f, 3.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}) * CreateModelMatrix ();
-    //renderer.Draw (mvp, VertexShader, PixelShader);
-    //Save (renderer.frameBuffer, "a.bmp");
+	Renderer renderer;
+	renderer.DrawModel (model);
+	SaveBmp (renderer.frameBuffer, WIDTH, HEIGHT, "output.bmp");
+	//Matrix4 mvp = CreateProjectMatrix (1.57f, 1024.0f / 768.0f, 0.1f, 1000.0f) * CreateViewMatrix ({4.0f, 3.0f, 3.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}) * CreateModelMatrix ();
 	return 0;
 }
